075662c656d92d0f02edaa7c632ec7ea3e63e7a9
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-07-10 13:28:27 +0530

    Time: 342 ms (60.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT CASE WHEN id % 2 = 1 AND id + 1 IN (SELECT id FROM Seat) THEN id + 1 WHEN id % 2 = 0 THEN id - 1 ELSE id END AS id, Student FROM Seat ORDER BY id ASC;
