732da87ae76c29f2270d1f1b118703faf2528989
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-07-19 16:08:39 +0530

    Time: 51 ms (71.82%) | Memory: 18.2 MB (72.30%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def maximalRectangle(self, matrix):
+
+        if not matrix or not matrix[0]:
+            return 0
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+        heights = [0] * cols
+        max_area = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+            max_area = max(max_area, self.largestRectangleArea(heights))
+
+        return max_area
+
+    def largestRectangleArea(self, heights):
+        stack = []
+        max_area = 0
+        heights.append(0)  # Sentinel
+
+        for i in range(len(heights)):
+            while stack and heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, h * width)
+            stack.append(i)
+
+        heights.pop()  # remove sentinel
+        return max_area
