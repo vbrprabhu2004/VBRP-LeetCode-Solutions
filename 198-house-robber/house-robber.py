861dd917fcd5bddf2c9e38e2051be9d7b866dc5b
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-07-24 10:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (15.43%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+
         prev1 = 0  # max up to i-2
         prev2 = 0  # max up to i-1
 
@@ -13,3 +10,16 @@ class Solution(object):
             prev1 = temp
 
         return prev2
+''' My understanding code
+
+class Solution(object):
+    def rob(self, nums):
+
+        even_sum, odd_sum = 0, 0
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                even_sum += nums[i]
+            else:
+                odd_sum += nums[i]
+        return max(even_sum, odd_sum)
+'''
