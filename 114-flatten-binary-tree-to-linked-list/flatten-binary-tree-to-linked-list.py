c75ade543521323ef4339f63ce704f58e12761db
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-08-04 23:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (39.69%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -1,16 +1,5 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution(object):
     def flatten(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: None Do not return anything, modify root in-place instead.
-        """
         self.prev = None  # global pointer to previous node
 
         def dfs(node):
