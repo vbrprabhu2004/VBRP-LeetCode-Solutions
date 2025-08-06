6be34018a45836a3ca8e84c9eb0c48e59bad8e6c
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-08-06 15:08:55 +0530

    Time: 118 ms (23.17%) | Memory: 14.4 MB (87.30%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def shortestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
         rev = s[::-1]
         for i in range(len(s) + 1):
             if s.startswith(rev[i:]):
