92249670b972165d741f0f9fca5e2e0a936fd1c2
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-08-30 21:37:26 +0530

    Time: 43 ms (93.33%) | Memory: 14.7 MB (7.94%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.py
@@ -0,0 +1,81 @@
+class Node(object):
+    def __init__(self, val=0):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class MyLinkedList(object):
+
+    def __init__(self):
+        # Sentinel nodes for easy handling of head/tail
+        self.head = Node(0)  
+        self.tail = Node(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0  # track length
+
+    def get(self, index):
+        if index < 0 or index >= self.size:
+            return -1
+
+        # Choose direction: from head or tail (optimization)
+        if index < self.size // 2:
+            curr = self.head.next
+            for _ in range(index):
+                curr = curr.next
+        else:
+            curr = self.tail.prev
+            for _ in range(self.size - index - 1):
+                curr = curr.prev
+
+        return curr.val
+
+    def addAtHead(self, val):
+        self._addNodeAfter(self.head, Node(val))
+
+    def addAtTail(self, val):
+        self._addNodeAfter(self.tail.prev, Node(val))
+
+    def addAtIndex(self, index, val):
+        if index < 0 or index > self.size:
+            return
+        if index == self.size:
+            self.addAtTail(val)
+        else:
+            # get node at position index
+            node = self._getNode(index)
+            self._addNodeAfter(node.prev, Node(val))
+
+    def deleteAtIndex(self, index):
+        if index < 0 or index >= self.size:
+            return
+        node = self._getNode(index)
+        self._deleteNode(node)
+
+    # Helper: add new node after prev
+    def _addNodeAfter(self, prev, node):
+        nxt = prev.next
+        prev.next = node
+        node.prev = prev
+        node.next = nxt
+        nxt.prev = node
+        self.size += 1
+
+    # Helper: remove a node
+    def _deleteNode(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+        self.size -= 1
+
+    # Helper: get node at index
+    def _getNode(self, index):
+        if index < self.size // 2:
+            curr = self.head.next
+            for _ in range(index):
+                curr = curr.next
+        else:
+            curr = self.tail.prev
+            for _ in range(self.size - index - 1):
+                curr = curr.prev
+        return curr
