1233d6311bef4a10163776afa0108e0cc251ae09
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-08-31 22:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (21.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,18 +1,12 @@
 class Solution(object):
-
     def isValid(self, s):
-        characters = []  
-        opening = ["(", "[", "{"]
-        closing = [")", "]", "}"]
-        
-        for item in s:
-            if item in opening:
-                characters.append(item)
-            elif item in closing:
-                pos = closing.index(item)
-                if len(characters) > 0 and characters[-1] == opening[pos]:
-                    characters.pop()
-                else:
+        stack = []
+        pairs = {')':'(', '}':'{', ']':'['}
+
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack.pop() != pairs[ch]:
                     return False
-        
-        return len(characters) == 0
+            else:
+                stack.append(ch)
+        return not stack
