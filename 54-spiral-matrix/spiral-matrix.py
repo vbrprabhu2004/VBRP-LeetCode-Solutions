bc1451bb5481913acced80f9704c8eb9ee488692
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-09-12 22:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (84.45%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,36 +1,29 @@
 class Solution(object):
     def spiralOrder(self, matrix):
-        result = []
-        
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
         if not matrix:
-            return result
+            return []
         
-        top = 0
-        bottom = len(matrix) - 1
-        left = 0
-        right = len(matrix[0]) - 1
-
-        while top <= bottom and left <= right:
-            # Traverse from left to right
-            for i in range(left, right + 1):
-                result.append(matrix[top][i])
-            top += 1
-
-            # Traverse downwards
-            for i in range(top, bottom + 1):
-                result.append(matrix[i][right])
-            right -= 1
-
-            # Traverse from right to left
-            if top <= bottom:
-                for i in range(right, left - 1, -1):
-                    result.append(matrix[bottom][i])
-                bottom -= 1
-
-            # Traverse upwards
-            if left <= right:
-                for i in range(bottom, top - 1, -1):
-                    result.append(matrix[i][left])
-                left += 1
-
-        return result
+        res = []
+        while matrix:
+            # 1. Take the first row
+            res += matrix.pop(0)
+            
+            # 2. Take the last column
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+            
+            # 3. Take the last row reversed
+            if matrix:
+                res += matrix.pop()[::-1]
+            
+            # 4. Take the first column reversed
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+        
+        return res
