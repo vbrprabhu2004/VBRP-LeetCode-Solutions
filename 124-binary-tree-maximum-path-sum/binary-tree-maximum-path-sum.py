a18fe286b58632cbdd8e222c8ba976d0419903b3
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-10-19 13:09:06 +0530

    Time: 27 ms (39.46%) | Memory: 25.4 MB (58.46%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -5,15 +5,10 @@ class Solution(object):
         def helper(node):
             if node is None:
                 return 0
-            
-            left_gain = max(helper(node.left), 0)
+            left_gain = max(helper(node.left),0)
             right_gain = max(helper(node.right),0)
-
             current_path_sum = node.val + left_gain + right_gain
-
             self.max_sum = max(self.max_sum, current_path_sum)
-
             return node.val + max(left_gain, right_gain)
-        
         helper(root)
         return self.max_sum
