84bdca88a909a2881fadd641e6a8232386490b15
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-10-19 14:38:16 +0530

    Time: 55 ms (16.12%) | Memory: 28.9 MB (33.03%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -1,18 +1,15 @@
 class Solution(object):
     def rangeSumBST(self, root, low, high):
-        sum1 = 0
-        res = []
+        self.sum1 = 0
         def inorder(self, root):
             if not root:
                 return
             inorder(self, root.left)
-            res.append(root.val)
+            #res.append(root.val)
+            if(root.val <= high and low <= root.val):
+                self.sum1 += root.val
             # 1 3 5 6 7 10 13 15 18
             # low = 6 high = 10
             inorder(self, root.right)
         inorder(self, root)
-    
-        for i in res:
-            if(i <= high and low <= i):
-                sum1 += i
-        return sum1
+        return self.sum1
