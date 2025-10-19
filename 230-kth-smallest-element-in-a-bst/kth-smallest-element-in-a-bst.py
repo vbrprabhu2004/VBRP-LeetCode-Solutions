e5b9394ef4252c53c7ed5635f6f538d434fb4a4e
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-10-19 16:40:21 +0530

    Time: 7 ms (25.90%) | Memory: 20.3 MB (25.27%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -1,25 +1,14 @@
 class Solution(object):
     def kthSmallest(self, root, k):
-       
-        self.count = 0         # Counter to track the number of nodes visited
-        self.result = None     # Variable to store the k-th smallest value
-
-        def inorder(node):
-            # Stop traversal if node is None or result is already found
-            if not node or self.result is not None:
-                return
-
-            # Traverse the left subtree
-            inorder(node.left)
-
-            # Visit the current node
+        self.count = 0
+        #self.temp = root.val
+        def inorder(self, root):
+            if not root:
+                return 0
+            inorder(self, root.left)
             self.count += 1
-            if self.count == k:
-                self.result = node.val
-                return
-
-            # Traverse the right subtree
-            inorder(node.right)
-
-        inorder(root)
-        return self.result
+            if (self.count == k):
+                self.temp = root.val
+            inorder(self, root.right)
+        inorder(self, root)
+        return self.temp
