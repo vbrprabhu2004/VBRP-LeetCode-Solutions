b76970a8e518ff2004b756103971ca8c361b010b
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-10-19 20:05:17 +0530

    Time: 26 ms (44.81%) | Memory: 25.5 MB (31.99%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -2,13 +2,15 @@ class Solution(object):
     def maxPathSum(self, root):
         self.max_sum = float('-inf')
 
-        def helper(node):
-            if node is None:
+        def depth(root):
+            if not root:
                 return 0
-            left_gain = max(helper(node.left),0)
-            right_gain = max(helper(node.right),0)
-            current_path_sum = node.val + left_gain + right_gain
-            self.max_sum = max(self.max_sum, current_path_sum)
-            return node.val + max(left_gain, right_gain)
-        helper(root)
+            left_gain = max(depth(root.left), 0)
+            right_gain = max(depth(root.right), 0)
+
+            consecutive_sum = root.val + left_gain + right_gain
+            self.max_sum = max(self.max_sum, consecutive_sum)
+            return root.val + max(left_gain, right_gain)
+        depth(root)
         return self.max_sum
+            
