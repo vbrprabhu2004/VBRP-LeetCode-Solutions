16f6d3e9b1d2a8894365d9b06cafe41867a83559
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-10-28 16:47:45 +0530

    Time: 8 ms (27.67%) | Memory: 13.1 MB (51.26%) - LeetSync

---
+++ 174-dungeon-game/dungeon-game.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def calculateMinimumHP(self, dungeon):
+        if not dungeon or not dungeon[0]:
+            return 0
+        
+        m, n = len(dungeon), len(dungeon[0])
+        
+        dp = [[0] * n for _ in range(m)]
+        dp[m - 1][n - 1] = max(1, 1 - dungeon[m - 1][n - 1])
+        for j in range(n - 2, -1, -1):
+            dp[m - 1][j] = max(1, dp[m - 1][j + 1] - dungeon[m - 1][j])
+        for i in range(m - 2, -1, -1):
+            dp[i][n - 1] = max(1, dp[i + 1][n - 1] - dungeon[i][n - 1])
+        for i in range(m - 2, -1, -1):
+            for j in range(n - 2, -1, -1):
+                min_health_on_exit = min(dp[i + 1][j], dp[i][j + 1])
+                dp[i][j] = max(1, min_health_on_exit - dungeon[i][j])
+    
+        return dp[0][0]
