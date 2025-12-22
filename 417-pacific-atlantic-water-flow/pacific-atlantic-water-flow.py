921bd50b00e7f538af1188dfe31f266d15db4774
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2025-12-23 01:31:26 +0530

    Time: 16 ms (99.10%) | Memory: 19.1 MB (94.90%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -6,33 +6,33 @@ class Solution:
         rows = len(heights)
         cols = len(heights[0])
         ans = []
-
+        
         def bfs(starts):
+            visited = [[False] * cols for _ in range(rows)]
             queue = deque(starts)
-            visited = [[False]*cols for _ in range(rows)]
 
             for r, c in starts:
                 visited[r][c] = True
-
+            
             while queue:
                 a, b = queue.popleft()
 
-                if b + 1 < cols and not visited[a][b+1] and heights[a][b+1] >= heights[a][b]:
+                if b + 1 < cols and not visited[a][b+1] and heights[a][b + 1] >= heights[a][b]:
                     visited[a][b+1] = True
-                    queue.append((a, b+1))
+                    queue.append((a, b + 1))    
 
-                if a + 1 < rows and not visited[a+1][b] and heights[a+1][b] >= heights[a][b]:
+                if a + 1 < rows and not visited[a+1][b] and heights[a + 1][b] >= heights[a][b]:
                     visited[a+1][b] = True
-                    queue.append((a+1, b))
+                    queue.append((a + 1, b))
 
-                if b - 1 >= 0 and not visited[a][b-1] and heights[a][b-1] >= heights[a][b]:
+                if b - 1 >= 0 and not visited[a][b-1] and heights[a][b - 1] >= heights[a][b]:
                     visited[a][b-1] = True
-                    queue.append((a, b-1))
-
-                if a - 1 >= 0 and not visited[a-1][b] and heights[a-1][b] >= heights[a][b]:
+                    queue.append((a, b - 1))
+                    
+                if a - 1 >= 0 and not visited[a-1][b] and heights[a - 1][b] >= heights[a][b]:
                     visited[a-1][b] = True
-                    queue.append((a-1, b))
-
+                    queue.append((a - 1, b))
+        
             return visited
 
         pacific_starts = []
@@ -45,13 +45,12 @@ class Solution:
         for j in range(cols):
             pacific_starts.append((0, j))
             atlantic_starts.append((rows - 1, j))
-
+        
         pacific = bfs(pacific_starts)
         atlantic = bfs(atlantic_starts)
 
         for i in range(rows):
             for j in range(cols):
                 if pacific[i][j] and atlantic[i][j]:
-                    ans.append([i, j])
-
+                    ans.append([i,j])
         return ans
