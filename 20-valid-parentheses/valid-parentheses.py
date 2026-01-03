feb551b07a9d719103f5b007ad4ab8131533b2f6
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2026-01-03 13:50:48 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (97.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,8 +1,7 @@
-class Solution(object):
-    def isValid(self, s):
+class Solution:
+    def isValid(self, s: str) -> bool:
         stack = []
-        pairs = {')':'(', '}':'{', ']':'['}
-
+        pairs = {')' : '(', '}' : '{', ']' : '['}
         for ch in s:
             if ch in pairs:
                 if not stack or stack.pop() != pairs[ch]:
