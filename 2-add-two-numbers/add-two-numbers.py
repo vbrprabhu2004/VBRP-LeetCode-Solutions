422dbc191357f31ac90b77d392b9454349a9fa6b
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2026-03-04 22:38:09 +0530

    Time: 2 ms (70.84%) | Memory: 19.3 MB (72.86%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,15 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        if not l1 and not l2:
-            return None
-        elif not l1:
-            return l2
-        elif not l2:
-            return l1
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-        a = l1.val + l2.val
-        p = ListNode(a % 10)
-        p.next = self.addTwoNumbers(l1.next, l2.next)
-        if a >= 10:
-            p.next = self.addTwoNumbers(p.next, ListNode(1))
-        return p
+            sum1 = v1 + v2 + carry
+            carry = sum1 // 10
+            sum1 = sum1 % 10
+            curr.next = ListNode(sum1)
+            curr = curr.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
