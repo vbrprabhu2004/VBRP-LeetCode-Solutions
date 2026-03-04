4edc247b946846ef00fca54ced8a34b9e1c5225a
Ramanath Prabhu V B <vbrprabhu2004@gmail.com>
2026-03-04 23:46:24 +0530

    Time: 46 ms (65.55%) | Memory: 20.1 MB (54.76%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -1,31 +1,37 @@
-class Solution(object):
-    def copyRandomList(self, head):
-        if not head:
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
             return None
+        
+        l1 = head
+        while l1 is not None:
+            l2 = Node(l1.val)
+            l2.next = l1.next
+            l1.next = l2
+            l1 = l2.next
+        newhead = head.next
 
-        # 1. Create new nodes and insert them after each original node
-        curr = head
-        while curr:
-            new_node = Node(curr.val)
-            new_node.next = curr.next
-            curr.next = new_node
-            curr = new_node.next
-
-        # 2. Assign random pointers for the copied nodes
-        curr = head
-        while curr:
-            if curr.random:
-                curr.next.random = curr.random.next
-            curr = curr.next.next
-
-        # 3. Separate the original and copied lists
-        curr = head
-        copy_head = head.next
-        while curr:
-            copy = curr.next
-            curr.next = copy.next
-            if copy.next:
-                copy.next = copy.next.next
-            curr = curr.next
-
-        return copy_head
+        l1 = head
+        while l1 is not None:
+            if l1.random is not None:
+                l1.next.random = l1.random.next
+            l1 = l1.next.next
+        
+        l1 = head
+        while l1 is not None:
+            l2 = l1.next
+            l1.next = l2.next
+            if l2.next is not None:
+                l2.next = l2.next.next
+            l1 = l1.next
+        return newhead
+                
